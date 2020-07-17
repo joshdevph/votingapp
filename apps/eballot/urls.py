b49140d10fd762ded31b4b22de4ca2fc9eb4c85e
jsondelosreyes <jasondelosreyes914@gmail.com>
2020-07-17 12:02:54 +0800

    update urls

--- apps/eballot/urls.py
+++ apps/eballot/urls.py
@@ -10,6 +10,10 @@ urlpatterns = [
     path('eballot_list/eballot_form/<int:id>', views.eballot_form, name='eballot_form'),
     path('eballot_list/eballot_form/cast_vote/<int:id>', views.cast_vote, name='cast_vote'),
     path('eballot_list/eballot_form/cast_remove/<int:id> <int:sh_id>', views.cast_remove, name='cast_remove'),
+    path('eballot_list/eballot_form/vote_summary/<str:id>', views.vote_summary, name='vote_summary'),
     path('result/', views.result, name='result'),
-    path('result/update_elected/<str:id>', views.update_elected, name='update_elected')
+    path('result/end_election/<str:id>', views.end_election, name='end_election'),
+    path('result/update_elected/<str:id>', views.update_elected, name='update_elected'),
+     path('select_election_report/', views.select_election_report, name='select_election_report')
+
 ]
